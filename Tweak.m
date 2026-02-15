957e17bec57bc73a2beccf3d039bbe3c75aea1ac
oc9201744-cmd <oc9201744@gmail.com>
2026-02-15 16:38:04 +0300

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -3,7 +3,7 @@
 #include <mach-o/dyld.h>
 #include <stdint.h>
 
-// --- DOBBY HEADER TANIMI ---
+// Dobby'yi dışarıdan tanıtıyoruz
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,21 +12,19 @@ extern "C" {
 }
 #endif
 
-// --- CONFIG ---
-#define TARGET_IMAGE "ShadowTrackerExtra"
-
-// --- HOOK MANTIĞI ---
+// --- HOOK FONKSİYONLARI ---
+// Rapor gönderen fonksiyonları buraya yönlendiriyoruz
 void *hook_AnoSDK_Report(void *arg1, void *arg2) {
-    // Veri gönderimini keser ve boş döner
-    return NULL; 
+    // NSLog(@"[Onurcan] Rapor engellendi.");
+    return NULL; // Hiçbir veri göndermeden geri dön
 }
 
-// --- BASE BULUCU ---
+// --- BASE HESAPLAMA ---
 uintptr_t get_game_base() {
     uint32_t count = _dyld_image_count();
     for (uint32_t i = 0; i < count; i++) {
         const char *name = _dyld_get_image_name(i);
-        if (name && strstr(name, TARGET_IMAGE)) {
+        if (name && strstr(name, "ShadowTrackerExtra")) {
             return (uintptr_t)_dyld_get_image_vmaddr_slide(i) + 0x100000000;
         }
     }
@@ -36,21 +34,29 @@ uintptr_t get_game_base() {
 // --- ANA GİRİŞ ---
 __attribute__((constructor))
 static void entry() {
-    // Oyunun tam yüklenmesi için 45 saniye bekle
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(45 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    // 60 saniye gecikme (En güvenli süre)
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(60 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         uintptr_t base = get_game_base();
         if (base > 0x100000000) {
             
-            // Pubg.txt'den gelen ofsetler
-            // DelReportData ve SetReportData fonksiyonlarını susturuyoruz
-            void *orig1;
-            DobbyHook((void *)(base + 0x23874), (void *)hook_AnoSDK_Report, (void **)&orig1);
+            // Pubg.txt'deki AnoSDK ofsetleri
+            void *orig1, *orig2;
             
-            void *orig2;
+            // _AnoSDKDelReportData3_0 adresi civarı
+            DobbyHook((void *)(base + 0x23874), (void *)hook_AnoSDK_Report, (void **)&orig1);
             DobbyHook((void *)(base + 0x23C74), (void *)hook_AnoSDK_Report, (void **)&orig2);
             
-            NSLog(@"[DobbyBypass] Ofsetler Hooklandı: 0x23874, 0x23C74");
+            NSLog(@"[Onurcan] Dobby Hooklar Çakıldı!");
+            
+            // Bildirim
+            dispatch_async(dispatch_get_main_queue(), ^{
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Onurcan Bypass" 
+                    message:@"Dobby Motoru Aktif: Raporlama Kapatıldı!" 
+                    preferredStyle:UIAlertControllerStyleAlert];
+                [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+                [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+            });
         }
     });
 }
