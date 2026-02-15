499c9ba7ecdc93337c0dfb166a863b43c536ed30
oc9201744-cmd <oc9201744@gmail.com>
2026-02-15 16:43:15 +0300

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -3,7 +3,7 @@
 #include <mach-o/dyld.h>
 #include <stdint.h>
 
-// Dobby'yi dışarıdan tanıtıyoruz
+// Dobby Header tanımı
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,14 +12,13 @@ extern "C" {
 }
 #endif
 
-// --- HOOK FONKSİYONLARI ---
-// Rapor gönderen fonksiyonları buraya yönlendiriyoruz
+// --- HOOK MANTIĞI ---
 void *hook_AnoSDK_Report(void *arg1, void *arg2) {
-    // NSLog(@"[Onurcan] Rapor engellendi.");
-    return NULL; // Hiçbir veri göndermeden geri dön
+    // Tencent raporlamayı burada durduruyoruz
+    return NULL; 
 }
 
-// --- BASE HESAPLAMA ---
+// --- BASE BULUCU ---
 uintptr_t get_game_base() {
     uint32_t count = _dyld_image_count();
     for (uint32_t i = 0; i < count; i++) {
@@ -34,7 +33,7 @@ uintptr_t get_game_base() {
 // --- ANA GİRİŞ ---
 __attribute__((constructor))
 static void entry() {
-    // 60 saniye gecikme (En güvenli süre)
+    // 60 saniye bekle (Sunucu senkronizasyonu için)
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(60 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         uintptr_t base = get_game_base();
@@ -43,20 +42,11 @@ static void entry() {
             // Pubg.txt'deki AnoSDK ofsetleri
             void *orig1, *orig2;
             
-            // _AnoSDKDelReportData3_0 adresi civarı
+            // Ofsetler: 0x23874 ve 0x23C74
             DobbyHook((void *)(base + 0x23874), (void *)hook_AnoSDK_Report, (void **)&orig1);
             DobbyHook((void *)(base + 0x23C74), (void *)hook_AnoSDK_Report, (void **)&orig2);
             
-            NSLog(@"[Onurcan] Dobby Hooklar Çakıldı!");
-            
-            // Bildirim
-            dispatch_async(dispatch_get_main_queue(), ^{
-                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Onurcan Bypass" 
-                    message:@"Dobby Motoru Aktif: Raporlama Kapatıldı!" 
-                    preferredStyle:UIAlertControllerStyleAlert];
-                [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
-                [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
-            });
+            NSLog(@"[Onurcan] Dobby Bypass Aktif Edildi.");
         }
     });
 }
