6ec15a600aed4d913d9479e95d1566bb18c3b066
oc9201744-cmd <oc9201744@gmail.com>
2026-02-15 17:15:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,14 @@
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-#include <mach-o/dyld.h>
-#include <stdint.h>
+#include <iostream>
 
-// Dobby fonksiyonunu dışarıdan tanıtıyoruz
-#ifdef __cplusplus
-extern "C" {
-#endif
-    int DobbyHook(void *function_address, void *replace_call, void **origin_call);
-#ifdef __cplusplus
-}
-#endif
+// Bu dosya dylib derlenirken sembolik olarak bulunur.
+// Asıl bypass kodların Tweak.m veya DobbyHookExample.mm içindedir.
 
-// --- HOOK MANTIĞI ---
-void *hook_AnoSDK_Report(void *arg1, void *arg2) {
-    // Güvenlik raporlamasını susturur
-    return NULL; 
+void placeholder_function() {
+    // Burası boş kalabilir
 }
 
-// --- BASE BULUCU ---
-uintptr_t get_game_base() {
-    uint32_t count = _dyld_image_count();
-    for (uint32_t i = 0; i < count; i++) {
-        const char *name = _dyld_get_image_name(i);
-        if (name && strstr(name, "ShadowTrackerExtra")) {
-            return (uintptr_t)_dyld_get_image_vmaddr_slide(i) + 0x100000000;
-        }
-    }
+// Eğer projeyi konsol uygulaması olarak test etmek istersen:
+int main() {
+    std::cout << "Dobby Hook Loader Ready" << std::endl;
     return 0;
-}
-
-// --- ANA GİRİŞ ---
-__attribute__((constructor))
-static void entry() {
-    // 60 saniye beklemek lobi banını önler
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(60 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-        uintptr_t base = get_game_base();
-        if (base > 0x100000000) {
-            
-            void *orig1, *orig2;
-            
-            // Pubg.txt'den aldığımız Anogs ofsetleri
-            DobbyHook((void *)(base + 0x23874), (void *)hook_AnoSDK_Report, (void **)&orig1);
-            DobbyHook((void *)(base + 0x23C74), (void *)hook_AnoSDK_Report, (void **)&orig2);
-            
-            NSLog(@"[Onurcan] Dobby Bypass Aktif!");
-        }
-    });
 }
