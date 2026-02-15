5255e3ad6138bc97e3d4f72d816e23b04a7dc188
oc9201744-cmd <oc9201744@gmail.com>
2026-02-15 19:34:30 +0300

    Update MemoryUtils.h

--- MemoryUtils.h
+++ MemoryUtils.h
@@ -1,36 +1,46 @@
+#ifndef MEMORY_UTILS_H
+#define MEMORY_UTILS_H
+
 #include <mach-o/dyld.h>
 #include <mach/mach.h>
 #include <sys/mman.h>
 #include <string.h>
 
-// 1. Canlı Base Adresini (ASLR dahil) otomatik bulur
-uintptr_t get_live_base() {
-    // 0. index her zaman ana uygulamanın (executable) header'ıdır
+// iOS SDK'da PROT_COPY bazen eksik olabilir, manuel tanımlıyoruz
+#ifndef PROT_COPY
+#define PROT_COPY 0x10
+#endif
+
+// Canlı Base Adresini (ASLR dahil) otomatik bulur
+static uintptr_t get_live_base() {
     return (uintptr_t)_dyld_get_image_header(0);
 }
 
-// 2. Güvenli Yazma (iOS 17 & iPhone 15 Pro Max Uyumlu)
-bool auto_patch(uintptr_t offset, const void *data, size_t size) {
+// Güvenli Yazma (iPhone 15 Pro Max & iOS 17/18 Uyumlu)
+static bool auto_patch(uintptr_t offset, const void *data, size_t size) {
     uintptr_t address = get_live_base() + offset;
     
-    // iPhone 15 Pro Max için 16KB Sayfa Hizalaması
+    // Sayfa Hizalaması
     size_t pageSize = PAGE_SIZE; 
     uintptr_t pageStart = address & ~(pageSize - 1);
     
-    // Yazma İzni Al (PROT_COPY iOS 17'de daha stabildir)
+    // Yazma İzni Al: PROT_COPY | PROT_READ | PROT_WRITE
     if (mprotect((void *)pageStart, pageSize, PROT_READ | PROT_WRITE | PROT_COPY) != 0) {
+        // Eğer PROT_COPY başarısız olursa standart RWX dene
         mprotect((void *)pageStart, pageSize, PROT_READ | PROT_WRITE | PROT_EXEC);
     }
     
     // Yamayı uygula
     memcpy((void *)address, data, size);
     
-    // Korumayı geri yükle
+    // Korumayı geri yükle (Sadece Okuma ve Çalıştırma)
     mprotect((void *)pageStart, pageSize, PROT_READ | PROT_EXEC);
     return true;
 }
 
-// ARM64 Sabitleri
+// ARM64 Komutları
 const unsigned char arm64_ret[] = {0xC0, 0x03, 0x5F, 0xD6};
 const unsigned char arm64_mov_w0_0_ret[] = {0x00, 0x00, 0x80, 0x52, 0xC0, 0x03, 0x5F, 0xD6};
 const unsigned char arm64_mov_w0_1_ret[] = {0x20, 0x00, 0x80, 0x52, 0xC0, 0x03, 0x5F, 0xD6};
+
+#endif
