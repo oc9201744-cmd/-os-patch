ee5b1a741cca23547c2c1aa114af37ff70e3ef16
oc9201744-cmd <oc9201744@gmail.com>
2026-02-19 21:47:08 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,25 +1,29 @@
+#import <Foundation/Foundation.h>
+#import <mach-o/dyld.h>
 #include <string.h>
 #include <dlfcn.h>
-#import <Foundation/Foundation.h>
 
-// Loglama için kısa yol
+// 1. HATA ÇÖZÜMÜ: DobbyHook fonksiyonunu derleyiciye tanıtıyoruz
+extern "C" int DobbyHook(void *address, void *replace_call, void **origin_call);
+
+// Loglama Makrosu
 #define LOG(fmt, ...) NSLog(@"[AnogsBypass] " fmt, ##__VA_ARGS__)
 
-// Orijinal veriyi saklayacak tampon
-// Dosyandaki 0x4224 adresinin ilk 8 byte'ı: STP X29, X30, [SP,#-0x10]! ve MOV X29, SP
+// Orijinal veriyi saklayacak tampon (Analiz.txt 0x4224 adresindeki ilk 8 byte)
 unsigned char original_buffer[8] = {0xFD, 0x7B, 0xBF, 0xA9, 0xFD, 0x03, 0x00, 0x91}; 
 
 uintptr_t target_base = 0;
 int (*orig_memcmp)(const void *s1, const void *s2, size_t n);
 
+// Bütünlük kontrolünü kör eden fonksiyon
 int new_memcmp(const void *s1, const void *s2, size_t n) {
     uintptr_t addr1 = (uintptr_t)s1;
     uintptr_t addr2 = (uintptr_t)s2;
     uintptr_t target_addr = target_base + 0x4224;
 
-    // Eğer tarama bizim yamalı adresimize denk gelirse (Burası kör etme noktası)
+    // Eğer sistem bizim yamalı adresimizi taramaya kalkarsa
     if (addr1 == target_addr || addr2 == target_addr) {
-        LOG("!!! INTEGRITY CHECK YAKALANDI !!! Adres: 0x4224 taranıyor. Sahte veri gönderiliyor...");
+        LOG("!!! INTEGRITY CHECK YAKALANDI !!! Sahte veri gönderiliyor...");
         
         if (addr1 == target_addr) return orig_memcmp(original_buffer, s2, n);
         return orig_memcmp(s1, original_buffer, n);
@@ -30,17 +34,18 @@ int new_memcmp(const void *s1, const void *s2, size_t n) {
 
 __attribute__((constructor))
 static void setup_bypass() {
-    // 1. Yazdırma Aktif: Tweak'in yüklendiğini bildir
-    LOG("Bypass Dylib Yüklendi. Base aranıyor...");
+    LOG("Bypass Dylib Yüklendi. Sistem başlatılıyor...");
 
-    // Hedef kütüphaneyi bul (Örn: ShadowTrackerExtra veya anogs)
+    // 2. HATA ÇÖZÜMÜ: _dyld_get_image_vmaddr_slide kullanımı için doğru index
+    // 0 genellikle ana binary'dir (ShadowTrackerExtra)
     target_base = (uintptr_t)_dyld_get_image_vmaddr_slide(0); 
-    LOG("Base Adresi Bulundu: 0x%lx", target_base);
+    LOG("Base Adresi: 0x%lx", target_base);
 
-    // 2. Yazdırma Aktif: memcmp hooklanıyor mu kontrol et
-    if (DobbyHook((void *)memcmp, (void *)new_memcmp, (void **)&orig_memcmp) == 0) {
-        LOG("BAŞARILI: memcmp hooklandı. Tarayıcı artık kör.");
+    // Integrity Check'i kör etmek için memcmp'yi kancala
+    void *memcmp_ptr = dlsym(RTLD_DEFAULT, "memcmp");
+    if (memcmp_ptr && DobbyHook(memcmp_ptr, (void *)new_memcmp, (void **)&orig_memcmp) == 0) {
+        LOG("BAŞARILI: memcmp kancalandı. Tarayıcı kör edildi.");
     } else {
-        LOG("HATA: memcmp hooklanamadı!");
+        LOG("HATA: memcmp kancalanamadı!");
     }
 }
