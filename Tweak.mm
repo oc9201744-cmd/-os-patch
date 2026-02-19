c6096e0f13d8aecb8b0e5d7a9e9619df746926bb
oc9201744-cmd <oc9201744@gmail.com>
2026-02-19 23:24:04 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -2,7 +2,7 @@
 #import <mach-o/dyld.h>
 #include <string.h>
 #include <dlfcn.h>
-#include <stdint.h> // uint64_t için gerekli
+#include <stdint.h>
 
 extern "C" int DobbyHook(void *address, void *replace_call, void **origin_call);
 
@@ -12,26 +12,38 @@ size_t anogs_size = 0x300000;
 
 int (*orig_memcmp)(const void *s1, const void *s2, size_t n);
 
-// HATA ÇÖZÜLDÜ: __int64 yerine uint64_t kullanıyoruz
-void (*orig_sub_6D1E0)(uint64_t a1);
+// Fotoğraftaki gibi uint64_t (int64) kullanıyoruz
+typedef uint64_t _QWORD;
+typedef uint32_t _DWORD;
+typedef uint16_t _WORD;
 
+void (*orig_sub_6D1E0)(uint64_t a1);
 void new_sub_6D1E0(uint64_t a1) {
-    // Önce orijinal akışın structları kurmasına izin ver
+    // 1. Önce orijinali çalıştır ki gerekli alanlar bellekte oluşsun
     if (orig_sub_6D1E0) {
         orig_sub_6D1E0(a1);
     }
+
+    // 2. FOTOĞRAFTAKİ MANTIK: Tüm raporlama offsetlerini sıfırla
+    // Senin paylaştığın sub_6D1E0 içindeki offsetlere göre:
+    *(_QWORD *)(a1 + 1000) = 0LL;
+    *(uint128_t *)(a1 + 984) = 0; // _OWORD karşılığı
+    *(_BYTE *)(a1 + 1040) = 0;
+    *(_WORD *)(a1 + 1360) = 0;
     
-    // Heartbeat ve Veri Değiştirme raporlarını susturma
-    // ACE / Anogs veri yapıları üzerinde doğrudan bellek manipülasyonu
-    *(uint8_t *)(a1 + 1362) = 0; // force_hb iptal
-    *(uint32_t *)(a1 + 328) = 0;  // Hata sayacı sıfırla
+    // Fotoğraftaki adamın yaptığı gibi kritik bayrakları temizle
+    *(_BYTE *)(a1 + 1362) = 0; // force_hb (Crash sebebi genelde buydu, şimdi safe)
+    *(_DWORD *)(a1 + 328) = 0; // Hata sayacı
+    
+    // Eğer fotoğraftaki sub_18E644 senin analizindekiyle aynıysa bunları da ekle:
+    // *(_QWORD *)(a1 + 104) = 0LL;
+    // *(_QWORD *)(a1 + 912) = 0LL;
 }
 
-// Bütünlük kontrolünü (Integrity) kör eden fonksiyon
+// Memcmp hook'un aynı kalıyor, Dobby izlerini gizlemek için şart
 int new_memcmp(const void *s1, const void *s2, size_t n) {
     uintptr_t addr1 = (uintptr_t)s1;
     uintptr_t addr2 = (uintptr_t)s2;
-
     if (anogs_base != 0 && anogs_backup != NULL) {
         if (addr1 >= anogs_base && addr1 < (anogs_base + anogs_size)) {
             size_t offset = addr1 - anogs_base;
@@ -45,51 +57,22 @@ int new_memcmp(const void *s1, const void *s2, size_t n) {
     return orig_memcmp(s1, s2, n);
 }
 
-void show_stealth_popup() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(7 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *win = nil;
-        if (@available(iOS 13.0, *)) {
-            for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-                if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
-                    win = ((UIWindowScene *)scene).windows.firstObject;
-                    break;
-                }
-            }
-        }
-        if (!win) win = [UIApplication sharedApplication].windows.firstObject;
-
-        if (win.rootViewController) {
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"System" 
-                                                                           message:@"Stealth & ACE Mode Active\nIntegrity Blinded" 
-                                                                    preferredStyle:UIAlertControllerStyleAlert];
-            [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
-            [win.rootViewController presentViewController:alert animated:YES completion:nil];
-        }
-    });
-}
-
 __attribute__((constructor))
 static void global_init() {
-    // 1. Önce memcmp hookla (Dobby'nin yapacağı değişiklikleri gizlemek için)
     void *m_ptr = dlsym(RTLD_DEFAULT, "memcmp");
     if (m_ptr) {
         DobbyHook(m_ptr, (void *)new_memcmp, (void **)&orig_memcmp);
     }
 
-    // 2. Anogs / ACE modülünü tara
     for (uint32_t i = 0; i < _dyld_image_count(); i++) {
         const char *name = _dyld_get_image_name(i);
         if (name && (strstr(name, "anogs") || strstr(name, "ace_cs2"))) {
             anogs_base = (uintptr_t)_dyld_get_image_vmaddr_slide(i);
-            
-            // Orijinal halini belleğe yedekle (Kör tarama için)
             anogs_backup = malloc(anogs_size);
             memcpy(anogs_backup, (void *)anogs_base, anogs_size);
             
-            // 3. ACE Raporlama Fonksiyonunu Hookla (0x6D1E0)
+            // Kancayı atıyoruz
             DobbyHook((void *)(anogs_base + 0x6D1E0), (void *)new_sub_6D1E0, (void **)&orig_sub_6D1E0);
-            
-            show_stealth_popup();
             break;
         }
     }
