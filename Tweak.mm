bd5d4c088265d485171cdfd7b2e622d387cb6b20
oc9201744-cmd <oc9201744@gmail.com>
2026-02-21 13:03:03 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,71 +1,58 @@
 #import <Foundation/Foundation.h>
 #import <mach-o/dyld.h>
-#include <string.h>
-#include <dlfcn.h>
-#include <stdint.h>
+#import <UIKit/UIKit.h>
+#import <dlfcn.h>
 
-// DobbyHook fonksiyonunu dışarıdan alıyoruz
-extern "C" int DobbyHook(void *address, void *replace_call, void **origin_call);
-
-// Loglama Makrosu
-#define LOG(fmt, ...) NSLog(@"[AnogsBypass] " fmt, ##__VA_ARGS__)
-
-// Analiz.txt 0x4224 adresindeki orijinal veriler (Integrity Check için)
-unsigned char original_buffer[8] = {0xFD, 0x7B, 0xBF, 0xA9, 0xFD, 0x03, 0x00, 0x91}; 
-
-uintptr_t target_base = 0;
-int (*orig_memcmp)(const void *s1, const void *s2, size_t n);
-
-// --- Bütünlük kontrolünü kör eden kanca ---
-int new_memcmp(const void *s1, const void *s2, size_t n) {
-    uintptr_t addr1 = (uintptr_t)s1;
-    uintptr_t addr2 = (uintptr_t)s2;
-    
-    if (target_base != 0) {
-        // Yamaladığın yerin adresi (Base + Offset)
-        uintptr_t target_addr = target_base + 0x4224;
-
-        // Eğer sistem bizim yamalı adresimizi taramaya kalkarsa orijinal veriyi gösteriyoruz
-        if (addr1 == target_addr || addr2 == target_addr) {
-            LOG("!!! INTEGRITY CHECK YAKALANDI !!! Orijinal veri taklit ediliyor...");
-            if (addr1 == target_addr) return orig_memcmp(original_buffer, s2, n);
-            return orig_memcmp(s1, original_buffer, n);
-        }
-    }
-    return orig_memcmp(s1, s2, n);
-}
-
-// --- Anogs Yüklendiğinde Çalışacak Fonksiyon (Hatasız Versiyon) ---
-static void on_image_load(const struct mach_header *mh, intptr_t slide) {
-    uint32_t count = _dyld_image_count();
-    for (uint32_t i = 0; i < count; i++) {
-        // Sistemdeki mevcut header'ı bulup ismini kontrol ediyoruz
-        if (_dyld_get_image_header(i) == mh) {
-            const char *name = _dyld_get_image_name(i);
-            if (name && strstr(name, "anogs")) {
-                target_base = (uintptr_t)slide;
-                LOG("🔥 ANOGS YAKALANDI! Base (Slide): 0x%lx", (long)target_base);
+// --- BİLDİRİM FONKSİYONU ---
+void force_show_alert(NSString *msg) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindow *window = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    window = scene.windows.firstObject;
+                    break;
+                }
             }
-            break;
         }
-    }
+        if (!window) window = [UIApplication sharedApplication].keyWindow;
+        if (!window) window = [UIApplication sharedApplication].windows.firstObject;
+
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"V4 BYPASS" 
+                                                                       message:msg 
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
+        
+        // Root view controller yoksa pencereye ekle (en garanti yol)
+        UIViewController *rootVC = window.rootViewController;
+        if (rootVC) {
+            [rootVC presentViewController:alert animated:YES completion:nil];
+        }
+    });
 }
 
-// --- Giriş Noktası ---
+// --- OTOMATİK ÇALIŞAN KISIM (CONSTRUCTOR) ---
+// Dylib yüklendiği an burası tetiklenir, protokole ihtiyaç duymaz!
 __attribute__((constructor))
-static void setup_bypass() {
-    LOG("Bypass Dylib Yüklendi.");
-
-    // 1. memcmp kancasını hemen at
-    void *memcmp_ptr = dlsym(RTLD_DEFAULT, "memcmp");
-    if (memcmp_ptr) {
-        if (DobbyHook(memcmp_ptr, (void *)new_memcmp, (void **)&orig_memcmp) == 0) {
-            LOG("BAŞARILI: memcmp kancalandı.");
-        } else {
-            LOG("HATA: memcmp kancalanamadı!");
-        }
-    }
-
-    // 2. Anogs'un yüklenmesini izle
-    _dyld_register_func_for_add_image(on_image_load);
+static void initialize() {
+    NSLog(@"[AnogsBypass] Dylib Belleğe Yüklendi!");
+    
+    // Oyunun yüklenmesini bekle (3 saniye sonra bildirimi bas)
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        force_show_alert(@"V4 Aktif: Bypass Hazır!");
+    });
 }
+
+// V4 şablonu hata vermesin diye boş protokol sınıfları durmaya devam etsin
+@protocol HackProtocol
+- (NSString *)getAppName;
+- (BOOL)hack;
+@end
+
+@interface DevHack : NSObject <HackProtocol>
+@end
+
+@implementation DevHack
+- (NSString *)getAppName { return @"com.tencent.ig"; }
+- (BOOL)hack { return YES; }
+@end
