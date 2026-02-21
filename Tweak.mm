2d2e97701ecfa2a879157e78e0bf3ce89bc9e2ad
oc9201744-cmd <oc9201744@gmail.com>
2026-02-21 22:02:21 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,65 +1,42 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
+#include "KittyMemory/KittyMemory.hpp"
+#include "KittyMemory/MemoryPatch.hpp"
 
-// Dobby'yi en yalın haliyle tanıtıyoruz
-#ifdef __cplusplus
-extern "C" {
-#endif
-    int DobbyHook(void *address, void *replace_call, void **origin_call);
-    int DobbyCodePatch(void *address, uint8_t *buffer, uint32_t buffer_size);
-#ifdef __cplusplus
-}
-#endif
+// Dobby'yi kancalar için tanıtıyoruz
+extern "C" int DobbyHook(void *address, void *replace_call, void **origin_call);
 
-// Mesaj Basma Fonksiyonu
-void show_v5_alert(NSString *msg) {
+// Mesaj Kutusu
+void show_msg(NSString *msg) {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow *window = [UIApplication sharedApplication].windows.firstObject;
         if (window.rootViewController) {
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"V5 BYPASS" 
-                                                                           message:msg 
-                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"V5 AUTO KITTY" message:msg preferredStyle:UIAlertControllerStyleAlert];
             [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
             [window.rootViewController presentViewController:alert animated:YES completion:nil];
         }
     });
 }
 
-// Hooklar ve Orijinalleri
-static int (*orig_sub_19D98)(void* a1, void* a2);
-int hook_sub_19D98(void* a1, void* a2) { return 0; }
+// Global yama değişkeni
+MemoryPatch bypassPatch;
 
-static void* (*orig_sub_10C24)(void* a1);
-void* hook_sub_10C24(void* a1) { return orig_sub_10C24(a1); }
-
-static void (*orig_sub_19DF8)(void);
-void hook_sub_19DF8(void) { return; }
-
-static void (*orig_sub_4A130)(void);
-void hook_sub_4A130(void) { return; }
-
-// --- Ana Başlatıcı ---
-// %ctor yerine __attribute__((constructor)) kullanarak Substrate bağımlılığını iyice azaltıyoruz
 __attribute__((constructor))
-static void initialize_bypass() {
-    // Bu log gelirse dylib başarıyla yüklenmiştir
-    NSLog(@"[V5_LOG] Dylib sisteme girdi!");
-
+static void start() {
+    // Oyunun yüklenmesi için kısa bir bekleme
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
+        // ASLR'siz gerçek adresi KittyMemory kendi hesaplayabilir ama biz garantici olalım
         uintptr_t base = (uintptr_t)_dyld_get_image_header(0);
 
-        // ASLR + Hooklar
-        DobbyHook((void*)(base + 0x19D98), (void*)hook_sub_19D98, (void**)&orig_sub_19D98);
-        DobbyHook((void*)(base + 0x10C24), (void*)hook_sub_10C24, (void**)&orig_sub_10C24);
-        DobbyHook((void*)(base + 0x19DF8), (void*)hook_sub_19DF8, (void**)&orig_sub_19DF8);
-        DobbyHook((void*)(base + 0x4A130), (void*)hook_sub_4A130, (void**)&orig_sub_4A130);
-
-        // Byte Patch
-        uint8_t zero_ret[] = {0x00, 0x00, 0x80, 0xD2, 0xC0, 0x03, 0x5F, 0xD6}; 
-        DobbyCodePatch((void*)(base + 0x371E0), zero_ret, 8);
-
-        show_v5_alert(@"V5 BYPASS: AKTİF ✅");
+        // 0x371E0 adresine "MOV X0, #0; RET" yamasını uygula
+        bypassPatch = MemoryPatch::createWithBytes(base + 0x371E0, "\x00\x00\x80\xD2\xC0\x03\x5F\xD6", 8);
+        
+        if(bypassPatch.Modify()) {
+             show_msg(@"KittyMemory Otomatik Kuruldu ve Yamaladı! ✅");
+        } else {
+             NSLog(@"[KITTY] Yama başarısız!");
+        }
     });
 }
