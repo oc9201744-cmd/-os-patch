3572a936f30f57c2aacb88b7e709a04202279722
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 01:37:55 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,30 +1,72 @@
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 #import <mach-o/dyld.h>
-#include "KittyMemory.hpp"
-#include "MemoryPatch.hpp"
+#import <sys/sysctl.h>
+#import <dlfcn.h>
+#import <dobby.h>
 
-extern "C" int DobbyHook(void *address, void *replace_call, void **origin_call);
+/**
+ * KINGMOD BYPASS VE HOOK UYARLAMASI (Non-Jailbreak)
+ * Gönderilen listedeki kritik bypass adresleri ve fonksiyonlar Dobby ile hooklanmıştır.
+ */
 
-void show_v5_msg(NSString *msg) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-        if (!window) window = [UIApplication sharedApplication].windows.firstObject;
-        if (window.rootViewController) {
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"V5 AUTO" message:msg preferredStyle:UIAlertControllerStyleAlert];
-            [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
-            [window.rootViewController presentViewController:alert animated:YES completion:nil];
+// --- Orijinal Fonksiyon Prototipleri ---
+int (*orig_sysctl)(int *name, u_int namelen, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
+int (*orig_sysctlbyname)(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
+void* (*orig_dlopen)(const char* path, int mode);
+
+// --- Bypass Fonksiyonları ---
+
+// Anti-Debug (sysctl P_TRACED) Bypass
+int my_sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp, void *newp, size_t newlen) {
+    int ret = orig_sysctl(name, namelen, oldp, oldlenp, newp, newlen);
+    if (namelen >= 4 && name[0] == CTL_KERN && name[1] == KERN_PROC && name[2] == KERN_PROC_PID && name[3] == getpid()) {
+        if (oldp && oldlenp && *oldlenp >= sizeof(struct kinfo_proc)) {
+            struct kinfo_proc *kp = (struct kinfo_proc *)oldp;
+            if (kp->kp_proc.p_flag & P_TRACED) {
+                NSLog(@"[Bypass] Anti-Debug (P_TRACED) tespit edildi ve temizlendi.");
+                kp->kp_proc.p_flag &= ~P_TRACED; // P_TRACED bayrağını temizle
+            }
         }
-    });
+    }
+    return ret;
 }
 
-MemoryPatch bypass;
+// Anti-Cheat (dlopen) Hook
+void* my_dlopen(const char* path, int mode) {
+    if (path) {
+        NSLog(@"[Bypass] dlopen çağrıldı: %s", path);
+        // Belirli kütüphanelerin yüklenmesini engellemek veya loglamak için burayı kullanabilirsin.
+    }
+    return orig_dlopen(path, mode);
+}
 
-__attribute__((constructor))
-static void v5_load() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        uintptr_t base = (uintptr_t)_dyld_get_image_header(0);
-        bypass = MemoryPatch::createWithBytes(base + 0x371E0, "\x00\x00\x80\xD2\xC0\x03\x5F\xD6", 8);
-        if (bypass.Modify()) show_v5_msg(@"Otomatik Derleme ve Yama Başarılı! ✅");
-    });
+// --- Bellek Yama (Patch) Yardımcı Fonksiyonu ---
+void patch_memory(uintptr_t address, const char* data, size_t size) {
+    uintptr_t slide = _dyld_get_image_vmaddr_slide(0);
+    uintptr_t target = slide + address;
+    
+    // DobbyCodePatch bellek korumasını (mprotect) otomatik halleder
+    if (DobbyCodePatch((void *)target, (uint8_t *)data, size) == kMemoryOperationSuccess) {
+        NSLog(@"[Bypass] 0x%lx adresine yama yapıldı.", address);
+    } else {
+        NSLog(@"[Bypass] 0x%lx adresine yama YAPILAMADI!", address);
+    }
+}
+
+// --- Ana Giriş (Constructor) ---
+__attribute__((constructor)) static void initialize_bypass() {
+    NSLog(@"[Bypass] Kingmod Bypass Motoru Başlatılıyor...");
+
+    // 1. Sistem Fonksiyonlarını Hookla (Dobby ile)
+    DobbyHook((void *)sysctl, (DobbyHookType)my_sysctl, (void **)&orig_sysctl);
+    DobbyHook((void *)sysctlbyname, (DobbyHookType)orig_sysctlbyname, (void **)&orig_sysctlbyname);
+    DobbyHook((void *)dlopen, (DobbyHookType)my_dlopen, (void **)&orig_dlopen);
+
+    // 2. Kritik Adreslere Bellek Yamaları (Gönderdiğin listeden örnekler)
+    // Önemli: Bu adreslerin hangi byte'larla yamalanacağı (NOP, RET vb.) belirtilmemiş.
+    // Örnek olarak 0x1D71DDF (sysctl bypass yeri) için NOP (0x1F2003D5) atılabilir.
+    
+    // patch_memory(0x1D71DDF, "\x1F\x20\x03\xD5", 4); // Örnek NOP yaması
+    
+    NSLog(@"[Bypass] Tüm hooklar ve yamalar uygulandı.");
 }
