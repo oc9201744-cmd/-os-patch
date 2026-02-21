1bbdb1e3fae2942cfe9bf4889f40ff862a46873a
oc9201744-cmd <oc9201744@gmail.com>
2026-02-21 13:06:23 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,12 +1,10 @@
-#import <Foundation/Foundation.h>
-#import <mach-o/dyld.h>
 #import <UIKit/UIKit.h>
-#import <dlfcn.h>
 
-// --- BİLDİRİM FONKSİYONU ---
-void force_show_alert(NSString *msg) {
+void show_modern_alert(NSString *msg) {
     dispatch_async(dispatch_get_main_queue(), ^{
         UIWindow *window = nil;
+        
+        // iOS 13+ için modern pencere bulma yöntemi
         if (@available(iOS 13.0, *)) {
             for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
                 if (scene.activationState == UISceneActivationStateForegroundActive) {
@@ -15,44 +13,18 @@ void force_show_alert(NSString *msg) {
                 }
             }
         }
-        if (!window) window = [UIApplication sharedApplication].keyWindow;
-        if (!window) window = [UIApplication sharedApplication].windows.firstObject;
-
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"V4 BYPASS" 
-                                                                       message:msg 
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
         
-        // Root view controller yoksa pencereye ekle (en garanti yol)
-        UIViewController *rootVC = window.rootViewController;
-        if (rootVC) {
-            [rootVC presentViewController:alert animated:YES completion:nil];
+        // Eğer hala bulunamadıysa (Eski iOS veya fallback)
+        if (!window) {
+            window = [UIApplication sharedApplication].windows.firstObject;
+        }
+
+        if (window.rootViewController) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"V4 Bypass" 
+                                                                           message:msg 
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
+            [window.rootViewController presentViewController:alert animated:YES completion:nil];
         }
     });
 }
-
-// --- OTOMATİK ÇALIŞAN KISIM (CONSTRUCTOR) ---
-// Dylib yüklendiği an burası tetiklenir, protokole ihtiyaç duymaz!
-__attribute__((constructor))
-static void initialize() {
-    NSLog(@"[AnogsBypass] Dylib Belleğe Yüklendi!");
-    
-    // Oyunun yüklenmesini bekle (3 saniye sonra bildirimi bas)
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        force_show_alert(@"V4 Aktif: Bypass Hazır!");
-    });
-}
-
-// V4 şablonu hata vermesin diye boş protokol sınıfları durmaya devam etsin
-@protocol HackProtocol
-- (NSString *)getAppName;
-- (BOOL)hack;
-@end
-
-@interface DevHack : NSObject <HackProtocol>
-@end
-
-@implementation DevHack
-- (NSString *)getAppName { return @"com.tencent.ig"; }
-- (BOOL)hack { return YES; }
-@end
