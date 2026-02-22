1cff0bcac61e8f6bc347113d59a6f8842a96ffeb
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 20:09:12 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -3,70 +3,56 @@
 #import <UIKit/UIKit.h>
 #include <dlfcn.h>
 #include <fcntl.h>
-#include <string.h> // strcasestr için
 #include "include/dobby.h"
 
-void* (*orig_dlopen)(const char* path, int mode);
+// Orijinal saklayıcılar
 int (*orig_open)(const char *path, int oflag, mode_t mode);
 
 void show_on_screen(NSString *message, CGFloat yPosition, UIColor *color) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        CGRect screenRect = [UIScreen mainScreen].bounds;
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, yPosition, screenRect.size.width, 30)];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, yPosition, [UIScreen mainScreen].bounds.size.width, 25)];
         label.text = message;
         label.textColor = color;
-        label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
+        label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
         label.textAlignment = NSTextAlignmentCenter;
-        label.font = [UIFont boldSystemFontOfSize:12];
+        label.font = [UIFont systemFontOfSize:10];
         label.layer.zPosition = 9999;
-        
-        UIWindow *window = nil;
-        if (@available(iOS 13.0, *)) {
-            for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-                if (scene.activationState == UISceneActivationStateForegroundActive) {
-                    window = ((UIWindowScene*)scene).windows.firstObject;
-                    break;
-                }
-            }
-        } else { window = [UIApplication sharedApplication].keyWindow; }
-        [window addSubview:label];
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[UIApplication sharedApplication].keyWindow addSubview:label];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [label removeFromSuperview];
         });
     });
 }
 
-// Büyük/Küçük harf duyarsız kontrol (Anogs veya anogs ikisini de yakalar)
-void* fake_dlopen(const char* path, int mode) {
-    if (path != NULL && strcasestr(path, "anogs")) {
-        NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
-        if (fakePath) {
-            show_on_screen(@"[DLOPEN] ANOGS YAKALANDI -> 002.BIN", 120, [UIColor greenColor]);
-            return orig_dlopen([fakePath UTF8String], mode);
-        }
-    }
-    return orig_dlopen(path, mode);
-}
-
+// BU KISIM TÜM TRAFİĞİ İZLER
 int fake_open(const char *path, int oflag, mode_t mode) {
-    if (path != NULL && strcasestr(path, "anogs")) {
-        NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
-        if (fakePath) {
-            show_on_screen(@"[OPEN] ANOGS YAKALANDI -> 002.BIN", 155, [UIColor cyanColor]);
-            return orig_open([fakePath UTF8String], oflag, mode);
+    if (path != NULL) {
+        NSString *currentPath = [NSString stringWithUTF8String:path];
+        
+        // Sadece önemli gördüğümüz uzantıları ekranda gösterelim (Ekran dolmasın diye)
+        if ([currentPath containsString:@"Anogs"] || [currentPath containsString:@"framework"] || [currentPath containsString:@"ShadowTracker"]) {
+            show_on_screen([NSString stringWithFormat:@"AÇILAN: %@", [currentPath lastPathComponent]], 100 + (arc4random() % 100), [UIColor orangeColor]);
+        }
+
+        // EĞER YAKALARSAK YÖNLENDİR
+        if ([currentPath.lowercaseString containsString:@"anogs"]) {
+            NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
+            if (fakePath) {
+                show_on_screen(@"!!! 002.BIN DEVREYE GİRDİ !!!", 70, [UIColor greenColor]);
+                return orig_open([fakePath UTF8String], oflag, mode);
+            }
         }
     }
     return orig_open(path, oflag, mode);
 }
 
 __attribute__((constructor))
-static void deep_hook_init() {
+static void scanner_init() {
     @autoreleasepool {
-        DobbyHook((void *)dlopen, (void *)fake_dlopen, (void **)&orig_dlopen);
         DobbyHook((void *)open, (void *)fake_open, (void **)&orig_open);
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            show_on_screen(@"[KINGMOD] HARF DUYARSIZ KANCA AKTIF", 50, [UIColor whiteColor]);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            show_on_screen(@"[KINGMOD] SCANNER MODU AKTIF", 40, [UIColor whiteColor]);
         });
     }
 }
