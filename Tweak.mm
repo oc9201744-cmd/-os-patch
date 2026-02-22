4adac0c7d655d738808b48b29477d68e79070aef
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 19:29:02 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,47 +1,44 @@
 #include <stdint.h>
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 #include <dlfcn.h>
 #include "dobby.h"
 
-// Orijinal fonksiyonu sakla
+// --- Orijinal dlopen'ı saklıyoruz ---
 void* (*orig_dlopen)(const char* path, int mode);
 
+// --- Framework Yükleme Simülasyonu ---
 void* fake_dlopen(const char* path, int mode) {
     if (path != NULL) {
-        // Log: Ne yükleniyor görelim
-        NSLog(@"[KINGMOD] Yüklenmeye çalışılan: %s", path);
-
+        // Oyun "Frameworks/anogs.framework/anogs" dosyasını çağırdığında...
         if (strstr(path, "anogs.framework/anogs")) {
-            NSLog(@"[KINGMOD] Hedef yakalandı! 002.bin yönlendirmesi başlıyor...");
-
-            NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
-            if (fakePath) {
-                // MANUEL YÜKLEME YERİNE: Orijinal dlopen'ı sahte yolla çağırıyoruz.
-                // iOS bu dosyanın .bin olduğuna bakmaz, içindeki Mach-O yapısına bakar.
-                void *handle = orig_dlopen([fakePath UTF8String], mode);
+            NSLog(@"[KINGMOD] Framework isteği yakalandı: %s", path);
+            
+            // 002.bin dosyasının yolunu alıyoruz
+            NSString *fakeFrameworkPath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
+            
+            if (fakeFrameworkPath) {
+                NSLog(@"[KINGMOD] 002.bin Framework olarak sisteme yediriliyor...");
+                
+                // Buradaki sihir: Orijinal dlopen'a bizim dosyamızı veriyoruz.
+                // Sistem bunu bir framework binary'si olarak belleğe haritalayacak.
+                void* handle = orig_dlopen([fakeFrameworkPath UTF8String], mode);
+                
                 if (handle) {
-                    NSLog(@"[KINGMOD] 002.bin BAŞARIYLA YÜKLENDİ!");
+                    NSLog(@"[KINGMOD] Framework başarıyla aktif edildi!");
                     return handle;
                 }
             }
         }
     }
+    // Geri kalan her şey normal yolunda devam etsin
     return orig_dlopen(path, mode);
 }
 
 __attribute__((constructor))
-static void setup_kingmod() {
-    // 1. Hook'u hemen at
-    DobbyHook((void *)dlopen, (void *)fake_dlopen, (void **)&orig_dlopen);
-
-    // 2. Dylib'in çalıştığını anlaman için hemen bir uyarı çıkartalım
-    // Eğer bu kutu gelmiyorsa, dylib enjekte olmamıştır.
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KINGMOD" 
-                                    message:@"Dylib Enjekte Edildi!\n002.bin Bekleniyor..." 
-                                    preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"Gazla Kanka" style:UIAlertActionStyleDefault handler:nil]];
-        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
-    });
+static void framework_loader_init() {
+    @autoreleasepool {
+        // dlopen fonksiyonuna kanca atarak framework yükleme sürecini ele geçiriyoruz
+        DobbyHook((void *)dlopen, (void *)fake_dlopen, (void **)&orig_dlopen);
+        NSLog(@"[KINGMOD] Framework yönlendirici hazır.");
+    }
 }
