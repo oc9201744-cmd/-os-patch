0c916945b5a1b51664ed141251b68de856d71722
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 17:55:02 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -4,45 +4,44 @@
 #include <dlfcn.h>
 #include "dobby.h"
 
-// Orijinal dlopen
+// Orijinal fonksiyonu sakla
 void* (*orig_dlopen)(const char* path, int mode);
 
 void* fake_dlopen(const char* path, int mode) {
     if (path != NULL) {
-        // Loglara her şeyi basıyoruz, ESign konsolundan takip et
-        NSLog(@"[KINGMOD_DEBUG] dlopen su dosyayi yukluyor: %s", path);
+        // Log: Ne yükleniyor görelim
+        NSLog(@"[KINGMOD] Yüklenmeye çalışılan: %s", path);
 
-        // Kontrolü genişletiyoruz: Büyük-küçük harf duyarsız yapalım
-        NSString *currentPath = [NSString stringWithUTF8String:path];
-        if ([currentPath.lowercaseString containsString:@"anogs"]) {
-            
-            // Dosyanın yerini bul
+        if (strstr(path, "anogs.framework/anogs")) {
+            NSLog(@"[KINGMOD] Hedef yakalandı! 002.bin yönlendirmesi başlıyor...");
+
             NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
-            
             if (fakePath) {
-                NSLog(@"[KINGMOD_SUCCESS] HEDEF YAKALANDI! Orijinal %s yerine %@ yukleniyor!", path, fakePath);
-                // Iste burada sihir gerceklesiyor: Orijinal yolu sahtesiyle degistiriyoruz
-                return orig_dlopen([fakePath UTF8String], mode);
-            } else {
-                NSLog(@"[KINGMOD_ERROR] 002.bin dosyasi IPA icinde bulunamadi!");
+                // MANUEL YÜKLEME YERİNE: Orijinal dlopen'ı sahte yolla çağırıyoruz.
+                // iOS bu dosyanın .bin olduğuna bakmaz, içindeki Mach-O yapısına bakar.
+                void *handle = orig_dlopen([fakePath UTF8String], mode);
+                if (handle) {
+                    NSLog(@"[KINGMOD] 002.bin BAŞARIYLA YÜKLENDİ!");
+                    return handle;
+                }
             }
         }
     }
     return orig_dlopen(path, mode);
 }
 
-static __attribute__((constructor)) void initialize_kingmod() {
-    // 1. dlopen'i kancala
+__attribute__((constructor))
+static void setup_kingmod() {
+    // 1. Hook'u hemen at
     DobbyHook((void *)dlopen, (void *)fake_dlopen, (void **)&orig_dlopen);
 
-    // 2. Ekrana bildirim bas (Dylib calisiyor mu kaniti)
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, [UIScreen mainScreen].bounds.size.width, 30)];
-        label.text = @"[ KINGMOD BYPASS LOADING... ]";
-        label.textColor = [UIColor greenColor];
-        label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.font = [UIFont boldSystemFontOfSize:14];
-        [[UIApplication sharedApplication].keyWindow addSubview:label];
+    // 2. Dylib'in çalıştığını anlaman için hemen bir uyarı çıkartalım
+    // Eğer bu kutu gelmiyorsa, dylib enjekte olmamıştır.
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KINGMOD" 
+                                    message:@"Dylib Enjekte Edildi!\n002.bin Bekleniyor..." 
+                                    preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Gazla Kanka" style:UIAlertActionStyleDefault handler:nil]];
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
     });
 }
