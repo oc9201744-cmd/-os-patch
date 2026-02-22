c3689d7948f105335ea6bd90f12befee725b77eb
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 21:14:56 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,58 +1,84 @@
 #include <stdint.h>
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#include <mach/mach.h>
+#include <mach/vm_map.h>
+#include <mach-o/dyld.h>
 #include <dlfcn.h>
-#include <fcntl.h>
-#include "include/dobby.h"
 
-// Orijinal saklayıcılar
-int (*orig_open)(const char *path, int oflag, mode_t mode);
-
-void show_on_screen(NSString *message, CGFloat yPosition, UIColor *color) {
+// --- Ekrana Bilgi Basan Fonksiyon ---
+void notify(NSString *msg, UIColor *color) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, yPosition, [UIScreen mainScreen].bounds.size.width, 25)];
-        label.text = message;
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 150, [UIScreen mainScreen].bounds.size.width, 30)];
+        label.text = msg;
         label.textColor = color;
-        label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
+        label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.7];
         label.textAlignment = NSTextAlignmentCenter;
-        label.font = [UIFont systemFontOfSize:10];
-        label.layer.zPosition = 9999;
+        label.font = [UIFont boldSystemFontOfSize:14];
         [[UIApplication sharedApplication].keyWindow addSubview:label];
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [label removeFromSuperview];
         });
     });
 }
 
-// BU KISIM TÜM TRAFİĞİ İZLER
-int fake_open(const char *path, int oflag, mode_t mode) {
-    if (path != NULL) {
-        NSString *currentPath = [NSString stringWithUTF8String:path];
-        
-        // Sadece önemli gördüğümüz uzantıları ekranda gösterelim (Ekran dolmasın diye)
-        if ([currentPath containsString:@"Anogs"] || [currentPath containsString:@"framework"] || [currentPath containsString:@"ShadowTracker"]) {
-            show_on_screen([NSString stringWithFormat:@"AÇILAN: %@", [currentPath lastPathComponent]], 100 + (arc4random() % 100), [UIColor orangeColor]);
-        }
-
-        // EĞER YAKALARSAK YÖNLENDİR
-        if ([currentPath.lowercaseString containsString:@"anogs"]) {
-            NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
-            if (fakePath) {
-                show_on_screen(@"!!! 002.BIN DEVREYE GİRDİ !!!", 70, [UIColor greenColor]);
-                return orig_open([fakePath UTF8String], oflag, mode);
-            }
+// --- ASIL OPERASYON: BELLEK YAMAMA ---
+void perform_memory_patch() {
+    uintptr_t targetBase = 0;
+    
+    // 1. Adım: Hafızada Anogs'un nerede olduğunu bul
+    uint32_t count = _dyld_image_count();
+    for (uint32_t i = 0; i < count; i++) {
+        const char *name = _dyld_get_image_name(i);
+        if (name && strstr(name, "Anogs.framework/Anogs")) {
+            targetBase = (uintptr_t)_dyld_get_image_header(i);
+            break;
         }
     }
-    return orig_open(path, oflag, mode);
+
+    if (targetBase == 0) {
+        notify(@"[HATA] ANOGS BELLEKTE BULUNAMADI!", [UIColor redColor]);
+        return;
+    }
+
+    // 2. Adım: 002.bin dosyasını oku
+    NSString *binPath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
+    NSData *binData = [NSData dataWithContentsOfFile:binPath];
+
+    if (!binData) {
+        notify(@"[HATA] 002.BIN DOSYASI YOK!", [UIColor orangeColor]);
+        return;
+    }
+
+    // 3. Adım: BELLEK KORUMASINI KIR (VM_PROTECT)
+    // Yazma izni alıyoruz (Read | Write | Execute)
+    kern_return_t kr;
+    mach_port_t task = mach_task_self();
+    vm_address_t address = (vm_address_t)targetBase;
+    vm_size_t size = binData.length;
+
+    kr = vm_protect(task, address, size, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    
+    if (kr == KERN_SUCCESS) {
+        // 4. Adım: VERİYİ ÜZERİNE YAZ (VM_WRITE)
+        kr = vm_write(task, address, (vm_offset_t)binData.bytes, (mach_msg_type_number_t)size);
+        
+        if (kr == KERN_SUCCESS) {
+            // 5. Adım: KORUMAYI ESKİ HALİNE GETİR
+            vm_protect(task, address, size, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
+            notify(@"[BAŞARILI] 002.BIN BELLEĞE ÇAKILDI!", [UIColor greenColor]);
+        } else {
+            notify(@"[HATA] VM_WRITE BAŞARISIZ!", [UIColor redColor]);
+        }
+    } else {
+        notify(@"[HATA] VM_PROTECT BAŞARISIZ!", [UIColor redColor]);
+    }
 }
 
 __attribute__((constructor))
-static void scanner_init() {
-    @autoreleasepool {
-        DobbyHook((void *)open, (void *)fake_open, (void **)&orig_open);
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            show_on_screen(@"[KINGMOD] SCANNER MODU AKTIF", 40, [UIColor whiteColor]);
-        });
-    }
+static void start() {
+    // Oyunun kütüphaneyi yüklemesi için 5 saniye bekle
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        perform_memory_patch();
+    });
 }
