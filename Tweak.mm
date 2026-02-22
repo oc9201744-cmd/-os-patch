249b083c125d184647410d7a4c7b32fbb88a43e7
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 19:47:59 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -2,12 +2,14 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #include <dlfcn.h>
+#include <fcntl.h>
 #include "dobby.h"
 
-// --- Orijinal dlopen'ı saklıyoruz ---
+// --- Orijinal Fonksiyon Saklayıcılar ---
 void* (*orig_dlopen)(const char* path, int mode);
+int (*orig_open)(const char *path, int oflag, mode_t mode);
 
-// --- Ekrana Bilgi Yazdırma Fonksiyonu ---
+// --- Ekrana Bilgi Yazdırma (Mavi/Yeşil Paneller) ---
 void show_on_screen(NSString *message, CGFloat yPosition, UIColor *color) {
     dispatch_async(dispatch_get_main_queue(), ^{
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, yPosition, [UIScreen mainScreen].bounds.size.width, 30)];
@@ -18,51 +20,45 @@ void show_on_screen(NSString *message, CGFloat yPosition, UIColor *color) {
         label.font = [UIFont boldSystemFontOfSize:14];
         label.layer.zPosition = 9999;
         [[UIApplication sharedApplication].keyWindow addSubview:label];
-        
-        // Yazı 5 saniye sonra kaybolsun
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [label removeFromSuperview];
         });
     });
 }
 
-// --- Framework Yükleme Simülasyonu ---
+// --- 1. Kapı: dlopen Kancası ---
 void* fake_dlopen(const char* path, int mode) {
-    if (path != NULL) {
-        // Ağı genişlettik! İçinde sadece "anogs" geçmesi yeterli.
-        if (strstr(path, "anogs")) {
-            
-            NSString *fakeFrameworkPath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
-            
-            if (fakeFrameworkPath) {
-                // Ekrana sarı renkte yakaladığımızı yazdırıyoruz
-                show_on_screen(@"[KINGMOD] ANOGS YAKALANDI! 002 YÜKLENİYOR...", 80, [UIColor yellowColor]);
-                
-                void* handle = orig_dlopen([fakeFrameworkPath UTF8String], mode);
-                
-                if (handle) {
-                    // Yükleme başarılıysa yeşil renkte onay veriyoruz
-                    show_on_screen(@"[KINGMOD] 002.BIN AKTİF EDİLDİ!", 115, [UIColor greenColor]);
-                    return handle;
-                } else {
-                    // Hata varsa kırmızı
-                    show_on_screen(@"[KINGMOD] YÜKLEME HATASI!", 115, [UIColor redColor]);
-                }
-            }
+    if (path != NULL && strstr(path, "anogs")) {
+        NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
+        if (fakePath) {
+            show_on_screen(@"[DLOPEN] ANOGS -> 002.BIN YÖNLENDİRİLDİ!", 120, [UIColor greenColor]);
+            return orig_dlopen([fakePath UTF8String], mode);
         }
     }
     return orig_dlopen(path, mode);
 }
 
+// --- 2. Kapı: open Kancası (Daha Garanti) ---
+int fake_open(const char *path, int oflag, mode_t mode) {
+    if (path != NULL && strstr(path, "anogs")) {
+        NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
+        if (fakePath) {
+            show_on_screen(@"[OPEN] ANOGS -> 002.BIN YÖNLENDİRİLDİ!", 155, [UIColor cyanColor]);
+            return orig_open([fakePath UTF8String],宣 oflag, mode);
+        }
+    }
+    return orig_open(path, oflag, mode);
+}
+
 __attribute__((constructor))
-static void framework_loader_init() {
+static void deep_hook_init() {
     @autoreleasepool {
+        // Fonksiyonlara kancaları atıyoruz
         DobbyHook((void *)dlopen, (void *)fake_dlopen, (void **)&orig_dlopen);
+        DobbyHook((void *)open, (void *)fake_open, (void **)&orig_open);
         
-        // Dylib'in oyuna başarıyla enjekte olduğunu gösteren ilk yazı (Mavi)
-        // Oyun açılır açılmaz en tepede bu çıkmalı!
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            show_on_screen(@"[KINGMOD] TWEAK ENJEKTE EDİLDİ", 45, [UIColor cyanColor]);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            show_on_screen(@"[KINGMOD] DERİN KANCA AKTİF - BEKLENİYOR...", 50, [UIColor whiteColor]);
         });
     }
 }
