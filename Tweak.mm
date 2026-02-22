af2b282371bc910ceb3f610a304c0fe3078b7720
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 21:26:46 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -2,12 +2,15 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #include <mach/mach.h>
+#include <mach/vm_map.h>
 #include <mach-o/dyld.h>
+#include <dlfcn.h> // Dl_info için bu ŞART!
+#include <string.h>
 
-// --- Bellek Yamama Fonksiyonu (Parametreli) ---
+// --- Bellek Yamama Fonksiyonu ---
 void apply_patch_to_address(uintptr_t header, const char* name) {
-    // Sadece Anogs'u hedef al
-    if (strstr(name, "Anogs.framework/Anogs")) {
+    // Büyük/küçük harf duyarsız kontrol (Anogs yakalamak için)
+    if (name && strcasestr(name, "Anogs.framework/Anogs")) {
         
         NSString *binPath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
         NSData *binData = [NSData dataWithContentsOfFile:binPath];
@@ -17,23 +20,36 @@ void apply_patch_to_address(uintptr_t header, const char* name) {
             vm_address_t address = (vm_address_t)header;
             vm_size_t size = binData.length;
 
-            // Korumayı aç (Read | Write | Copy)
-            if (vm_protect(task, address, size, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY) == KERN_SUCCESS) {
-                
-                // 002.bin içeriğini tam üzerine yaz
+            // 1. Korumayı aç (Read | Write | Copy)
+            kern_return_t kr = vm_protect(task, address, size, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+            
+            if (kr == KERN_SUCCESS) {
+                // 2. 002.bin içeriğini tam üzerine yaz (Memory Patch)
                 if (vm_write(task, address, (vm_offset_t)binData.bytes, (mach_msg_type_number_t)size) == KERN_SUCCESS) {
                     
-                    // Korumayı eski haline getir (Read | Execute)
+                    // 3. Korumayı eski haline getir (Read | Execute)
                     vm_protect(task, address, size, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
                     
                     // Ekranda Onay Mesajı
                     dispatch_async(dispatch_get_main_queue(), ^{
                         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 150, [UIScreen mainScreen].bounds.size.width, 40)];
-                        label.text = @"[!!!] ANOGS YAKALANDI VE YAMALANDI!";
+                        label.text = @"[!!!] ANOGS BELLEKTE YAMALANDI!";
                         label.textColor = [UIColor greenColor];
-                        label.backgroundColor = [UIColor blackColor];
+                        label.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8];
                         label.textAlignment = NSTextAlignmentCenter;
-                        [[UIApplication sharedApplication].keyWindow addSubview:label];
+                        label.font = [UIFont boldSystemFontOfSize:14];
+                        label.layer.zPosition = 9999;
+                        
+                        UIWindow *window = nil;
+                        if (@available(iOS 13.0, *)) {
+                            for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
+                                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                                    window = ((UIWindowScene*)scene).windows.firstObject;
+                                    break;
+                                }
+                            }
+                        } else { window = [UIApplication sharedApplication].keyWindow; }
+                        [window addSubview:label];
                     });
                 }
             }
@@ -41,16 +57,19 @@ void apply_patch_to_address(uintptr_t header, const char* name) {
     }
 }
 
-// --- Yeni Kütüphane Yüklendiğinde Çağrılan Callback ---
+// --- Yeni Kütüphane Yüklendiğinde Çağrılan Fonksiyon ---
 void on_image_load(const struct mach_header* mh, intptr_t vmaddr_slide) {
     Dl_info info;
+    // mh (header) adresinden dosya yolunu (name) buluyoruz
     if (dladdr(mh, &info)) {
-        apply_patch_to_address((uintptr_t)mh, info.dli_fname);
+        if (info.dli_fname != NULL) {
+            apply_patch_to_address((uintptr_t)mh, info.dli_fname);
+        }
     }
 }
 
 __attribute__((constructor))
 static void init() {
-    // Sisteme diyoruz ki: "Bundan sonra ne yüklenirse bana haber ver"
+    // Kütüphane yüklemelerini izlemeye al
     _dyld_register_func_for_add_image(on_image_load);
 }
