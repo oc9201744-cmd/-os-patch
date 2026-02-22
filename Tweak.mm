bdd97641b85f9e88fdd3d35253ea1751c3261224
oc9201744-cmd <oc9201744@gmail.com>
2026-02-22 16:17:01 +0300

    Update Tweak.mm

--- Tweak.mm
+++ Tweak.mm
@@ -1,34 +1,49 @@
 #include <stdint.h>
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #include <dlfcn.h>
 #include "dobby.h"
 
-// --- Orijinal dlopen ---
+// Orijinal dlopen
 void* (*orig_dlopen)(const char* path, int mode);
 
 void* fake_dlopen(const char* path, int mode) {
     if (path != NULL) {
-        // Log: Oyunun ne çağırdığını ESign loglarında görmek için
-        NSLog(@"[KINGMOD] dlopen çağrıldı: %s", path);
+        // Loglara her şeyi bas ki ESign'da ne yükleniyor gör
+        NSLog(@"[KINGMOD] dlopen: %s", path);
 
-        // Eğer yolun içinde "anogs" geçiyorsa (002.bin'in içindeki o gizli isim)
         if (strstr(path, "anogs")) {
-            // ESign ile Payload içine attığın 002.bin dosyasının gerçek yolunu al
-            NSString *manualPath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
-            
-            if (manualPath) {
-                NSLog(@"[KINGMOD] KRİTİK: anogs yakalandı! 002.bin'e yönlendiriliyor: %@", manualPath);
-                return orig_dlopen([manualPath UTF8String], mode);
+            NSString *fakePath = [[NSBundle mainBundle] pathForResource:@"002" ofType:@"bin"];
+            if (fakePath) {
+                NSLog(@"[KINGMOD] HEDEF BULUNDU! 002.bin yukleniyor...");
+                return orig_dlopen([fakePath UTF8String], mode);
             }
         }
     }
     return orig_dlopen(path, mode);
 }
 
-__attribute__((constructor))
-static void start_kingmod() {
-    // Dobby ile dlopen'ı en tepede yakala
+// Dylib yuklendigi an calisacak bolum
+static __attribute__((constructor)) void initialize_kingmod() {
+    // 1. Hemen dlopen'i kancala (Gecikme buraya konmaz!)
     DobbyHook((void *)dlopen, (void *)fake_dlopen, (void **)&orig_dlopen);
     
-    NSLog(@"[KINGMOD] Sistem hazır, anogs bekleniyor...");
+    NSLog(@"[KINGMOD] Hook atildi, 5 saniye sonra bildirim gelecek...");
+
+    // 2. Sadece görsel bildirim için 5 saniye bekle
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        // Ekranda uyarı çıkartma (UI thread üzerinde)
+        UIViewController *rootVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+        if (rootVC) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"KINGMOD" 
+                                        message:@"Bypass ve 002.bin Aktif!" 
+                                        preferredStyle:UIAlertControllerStyleAlert];
+            [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
+            [rootVC presentViewController:alert animated:YES completion:nil];
+        } else {
+            // Eğer ekran henüz hazır değilse loglara bas
+            NSLog(@"[KINGMOD] UI HENUZ HAZIR DEGIL AMA KOD CALISIYOR!");
+        }
+    });
 }
